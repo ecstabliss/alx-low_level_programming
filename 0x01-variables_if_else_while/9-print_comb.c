d81bc5844fa651b9dd96e8e50061b5a138228c14
ecstabliss <dija.uzumaki@gmail.com>
2023-06-16 13:45:08 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int digit = 0;
+
+    while (digit < 10)
+    {
+        putchar(digit + '0');  /* Print the current digit */
+
+        if (digit != 9)
+        {
+            putchar(',');  /* Print the comma separator */
+            putchar(' ');  /* Print the space separator */
+        }
+
+        digit++;
+    }
+
+    putchar('\n');  /* Print a newline character */
+
+    return (0);
+}
+
