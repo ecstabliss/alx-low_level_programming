762a084372d55bc167b10781d20b5fe57d846bd4
ecstabliss <dija.uzumaki@gmail.com>
2023-06-16 13:17:55 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+/* more headers goes there */
 
+/* betty style doc for function main goes there */
 int main(void) 
 {
     int n;
@@ -9,6 +11,7 @@ int main(void)
 
     srand(time(0));
     n = rand() - RAND_MAX / 2;
+    /* your code goes there */
 
     lastDigit = n % 10;
 
